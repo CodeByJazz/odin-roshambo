f3de2fab14f6399c618e95b70901e4616976eacc
Jazz Whitfield <jazzwhitfield123@gmail.com>
2022-04-22 14:59:03 -0400

    Create a function that plays a round of roshambo and increments the winners score by one. Add a scoreboard div to the homepage.

--- script.js
+++ script.js
@@ -1,51 +1,86 @@
+let playerChoice;
+let playerSelection;
 let playerScore = 0;
 let computerScore = 0;
 let drawScore = 0;
 
 //computerselection
 
-let computerPlay = () => {
-  const shapes = ["rock", "paper", "scissors"];
-  computerSelection = shapes[Math.floor(Math.random() * 3)];
-};
+const shapes = ["rock", "paper", "scissors"];
 
-//playerselection
+function computerPlay() {
+  return shapes[Math.floor(Math.random() * 3)];
+}
 
-document.getElementById("rock").addEventListener("click", function () {
-  let playerSelection = "rock";
+let computerSelection = computerPlay();
+
+let playerWinRound = "Player wins this round!";
+let computerWinRound = "Computer wins this round!";
+let draw = "It's a draw. Try again!";
+let playerWinGame = "Congratulations! You win the game!";
+let computerWinGame = "You Lose. Computer wins the game!";
+
+function playGame(playerSelection, computerSelection) {
+  computerSelection = computerPlay();
   console.log(playerSelection);
-});
+  console.log(computerSelection);
+  console.log(playerScore);
+  console.log(computerScore);
 
-document.getElementById("paper").addEventListener("click", function () {
-  let playerSelection = "paper";
-  console.log(playerSelection);
-});
-
-document.getElementById("scissors").addEventListener("click", function () {
-  let playerSelection = "scissors";
-  console.log(playerSelection);
-});
-
-// create a function that plays a round of Rock Paper Scissors
-
-function playRound(playerSelection, computerSelection) {
-  //rock choice
   if (playerSelection === computerSelection) {
+    drawScore++;
     return draw;
   } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    playerScore++;
     return playerWinRound;
   }
   //paper choice
   else if (playerSelection === "paper" && computerSelection === "rock") {
+    playerScore++;
     return playerWinRound;
   }
   //scissors choice
   else if (playerSelection === "scissors" && computerSelection === "paper") {
+    playerScore++;
     return playerWinRound;
   } else {
+    computerScore++;
     return computerWinRound;
   }
 }
+
+document.getElementById("rock").addEventListener("click", function () {
+  playGame("rock", computerSelection);
+});
+
+document.getElementById("paper").addEventListener("click", function () {
+  playGame("paper", computerSelection);
+});
+
+document.getElementById("scissors").addEventListener("click", function () {
+  playGame("scissors", computerSelection);
+});
+
+// create a function that plays a round of Rock Paper Scissors
+
+// let playRound = (playerSelection, computerSelection) => {
+//   //rock choice
+//   if (playerSelection === computerSelection) {
+//     return draw;
+//   } else if (playerSelection == "rock" && computerSelection == "scissors") {
+//     return playerWinRound;
+//   }
+//   //paper choice
+//   else if (playerSelection === "paper" && computerSelection === "rock") {
+//     return playerWinRound;
+//   }
+//   //scissors choice
+//   else if (playerSelection === "scissors" && computerSelection === "paper") {
+//     return playerWinRound;
+//   } else {
+//     return computerWinRound;
+//   }
+// };
 
 //create a variable for playerScore
 //create a variable for computerScore
@@ -57,11 +92,11 @@ function playRound(playerSelection, computerSelection) {
 
 //create a variable for the outcomes of each round/game
 
-let playerWinRound = "Player wins this round!";
-let computerWinRound = "Computer wins this round!";
-let draw = "It's a draw. Try again!";
-let playerWinGame = "Congratulations! You win the game!";
-let computerWinGame = "You Lose. Computer wins the game!";
+// let playerWinRound = "Player wins this round!";
+// let computerWinRound = "Computer wins this round!";
+// let draw = "It's a draw. Try again!";
+// let playerWinGame = "Congratulations! You win the game!";
+// let computerWinGame = "You Lose. Computer wins the game!";
 
 //create a loop that plays 5 rounds of roshambo,
 //asks player for a selection with the prompt function
